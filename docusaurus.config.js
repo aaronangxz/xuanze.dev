c343e86376a62c20f8d3989e571dc5b082c6235b
Xuan Ze <angxze@gmail.com>
2022-06-23 03:00:28 +0800

    Removed unstable gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,10 +34,10 @@ const config = {
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
                 },
-                gtag: {
-                    trackingID: 'G-5LDBR129KE',
-                    anonymizeIP: true,
-                },
+                // gtag: {
+                //     trackingID: 'G-5LDBR129KE',
+                //     anonymizeIP: true,
+                // },
             }),
         ],
     ],
