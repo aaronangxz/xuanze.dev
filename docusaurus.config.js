4fad0806346748359b5f36b76b30d503637cfa5f
Xuan Ze <angxze@gmail.com>
2022-09-16 13:41:27 +0800

    Sync sol-deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
     title: 'xuanze.sol',
     tagline: 'fmt.Println("Hello world.")',
     url: 'https://aaronangxz.github.io',
-    baseUrl: '/',
+    baseUrl: '/XZ-LeetCode-Journal/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.ico',
